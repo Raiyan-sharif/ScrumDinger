9234036c46f02cfac39d894c3845d76fb11139b2
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-05-10 10:08:21 +0600

    initial commit

---
+++ ScrumDinger/ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by BJIT on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
